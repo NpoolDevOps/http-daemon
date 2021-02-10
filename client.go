d0bfe834332b2935f4dc399a7e48b8812fcc8077
Zhao KK <kikakkz@hotmail.com>
2021-02-10 17:12:55 +0800

    Support client

---
+++ client.go
@@ -0,0 +1,30 @@
+package httpdaemon
+
+import (
+	"fmt"
+	log "github.com/EntropyPool/entropy-logger"
+	"github.com/go-resty/resty/v2"
+)
+
+var cli = resty.New()
+
+func R() *resty.Request {
+	return cli.R()
+}
+
+func ParseResponse(resp *resty.Response) (*ApiResp, error) {
+	apiResp, err := ParseResponseBody(resp.Body())
+	if err != nil {
+		log.Errorf(log.Fields{}, "< exchange api response error %v [%v] (%v)",
+			resp.Request.URL, resp.Request.QueryParam, err)
+		return nil, err
+	}
+
+	if apiResp.Code != 0 {
+		log.Errorf(log.Fields{}, "< exchange key error %v [%v] (%v)",
+			resp.Request.URL, resp.Request.QueryParam, apiResp.Msg)
+		return nil, fmt.Errorf("%v (%v)", apiResp.Msg, apiResp.Code)
+	}
+
+	return apiResp, nil
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/NpoolRD/http-daemon
 
 go 1.15
+
+require (
+	github.com/EntropyPool/entropy-logger v0.0.0-20210210082337-af230fd03ce7 // indirect
+	github.com/go-resty/resty/v2 v2.4.0 // indirect
+)
