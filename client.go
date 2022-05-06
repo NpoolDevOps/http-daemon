7943c2cae9a798279a98ce1dfde77e67b417e9a0
chen <531759321@qq.com>
2022-05-06 13:37:28 +0000

    update client.go

--- client.go
+++ client.go
@@ -2,7 +2,7 @@ package httpdaemon
 
 import (
 	"fmt"
-	log "github.com/EntropyPool/entropy-logger"
+	_ "github.com/EntropyPool/entropy-logger"
 	"github.com/go-resty/resty/v2"
 )
 
@@ -12,17 +12,21 @@ func R() *resty.Request {
 	return cli.R()
 }
 
+func Cli() *resty.Client {
+	return cli
+}
+
 func ParseResponse(resp *resty.Response) (*ApiResp, error) {
 	apiResp, err := ParseResponseBody(resp.Body())
 	if err != nil {
-		log.Errorf(log.Fields{}, "< api response parse error %v [%v] (%v)",
-			resp.Request.URL, resp.Request.QueryParam, err)
+		// log.Errorf(log.Fields{}, "< api response parse error %v [%v] (%v)",
+		//	resp.Request.URL, resp.Request.QueryParam, err)
 		return nil, err
 	}
 
 	if apiResp.Code != 0 {
-		log.Errorf(log.Fields{}, "< api response error %v [%v] (%v)",
-			resp.Request.URL, resp.Request.QueryParam, apiResp.Msg)
+		// log.Errorf(log.Fields{}, "< api response error %v [%v] (%v)",
+		//	resp.Request.URL, resp.Request.QueryParam, apiResp.Msg)
 		return nil, fmt.Errorf("%v (%v)", apiResp.Msg, apiResp.Code)
 	}
 
